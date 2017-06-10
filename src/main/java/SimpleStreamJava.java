9fde76f2eba539bcf0568773a6d446e7c1b3314f
Y Ramesh Rao <yrameshrao@gmail.com>
2017-06-10 17:46:48 +0530

    Optimized for readability

--- src/main/java/SimpleStreamJava.java
+++ src/main/java/SimpleStreamJava.java
@@ -3,18 +3,17 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SimpleStreamJava {
     public static void main(String[] args) throws IOException, URISyntaxException {
         Path readmeFile = Paths.get(SimpleStreamJava.class.getResource("README.md").toURI());
+        List<String> lines = Files.lines(readmeFile).collect(Collectors.toList());
 
-        long numberOfAs = countOfCharInFile(readmeFile, "a");
-        long numberOfBs = countOfCharInFile(readmeFile, "b");
+        long numberOfAs = lines.parallelStream().filter(line->line.contains("a")).count();
+        long numberOfBs = lines.parallelStream().filter(line->line.contains("b")).count();
 
         System.out.println(String.format("Lines with a:%d | Lines with b:%d", numberOfAs, numberOfBs));
-    }
-
-    private static long countOfCharInFile(Path readmeFilePath, String characterToSerach) throws IOException {
-        return Files.lines(readmeFilePath).parallel().filter(line->line.contains(characterToSerach)).count();
     }
 }
